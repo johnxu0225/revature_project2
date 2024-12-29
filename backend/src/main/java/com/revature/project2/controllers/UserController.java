975e5ee3c16bc2ad2a8bc87f1ca78b9a95209ca9
Naginipython <naginipython332@gmail.com>
2024-12-29 09:09:52 -0500

    Started login and register

---
+++ backend/src/main/java/com/revature/project2/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.revature.project2.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @PostMapping
+    public ResponseEntity<?> login() {
+        
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ backend/src/main/java/com/revature/project2/models/User.java
@@ -0,0 +1,67 @@
+package com.revature.project2.models;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Component
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    
+    private String role = "employee";
+
+    public User() {}
+
+    public User(String username, String password, String email, String firstName, String lastName, String role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+    }
+
+    public int getUserId() { return userId; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getEmail() { return email; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getRole() { return role; }
+
+    public void setUserId(int userId) { this.userId = userId; }
+    public void setUsername(String username) { this.username = username; }
+    public void setPassword(String password) { this.password = password; }
+    public void setEmail(String email) { this.email = email; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setRole(String role) { this.role = role; }
+
+    @Override
+    public String toString() {
+        return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", email=" + email
+                + ", firstName=" + firstName + ", lastName=" + lastName + ", role=" + role + "]";
+    }
+
+    
+}
