96f2f454ae88a520e4e99a54d060277bc732bdd3
Aleks <98655443+aleks-r74@users.noreply.github.com>
2025-01-10 17:22:29 -0500

    Added Dockerfile (#70)
    * added dockerfile for frontend and removed unused declarations/imports for npm run build to work

--- frontend/src/Components/Envelopes/EnvelopeList.tsx
+++ frontend/src/Components/Envelopes/EnvelopeList.tsx
@@ -1,6 +1,3 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
 import { EnvelopeListCard } from './EnvelopeListCard';
 
 interface History {
